72b36df803036e593981a0e86326339f9e7fbdf7
karthickmaestro <roughtest2@gmail.com>
2016-11-23 17:44:19 +0530

    commit change1

--- app/src/main/java/com/gitandroidfiles/MainActivity.java
+++ app/src/main/java/com/gitandroidfiles/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("Checking","Checking");
+        Log.e("Android testing","Android Testing");
     }
 }
