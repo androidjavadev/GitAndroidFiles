8e3e90becdded1fa6e95eb7289752b4206c1bef4
karthickmaestro <roughtest2@gmail.com>
2016-11-23 17:43:14 +0530

    commit change1

--- app/src/main/java/com/gitandroidfiles/MainActivity.java
+++ app/src/main/java/com/gitandroidfiles/MainActivity.java
@@ -11,11 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.e("Welcome","Welcome");
-        Log.e("Welcome","Welcome");
-        Log.e("Welcome","Welcome");
-        Log.e("Welcome","Welcome");
-        Log.e("Welcome","Welcome");
-        Log.e("Welcome","Welcome");
+        Log.e("Checking","Checking");
     }
 }
